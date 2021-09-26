08aa578c9170591c02831fd700d65a5d49e8c0f6
Maddy <ty.carlier@gmail.com>
2021-09-27 05:07:31 +1000

    Updated //fuck to newest discordjs version ( and added guild reactions intent )

--- src/shard.ts
+++ src/shard.ts
@@ -3,7 +3,7 @@
 import * as Discord from 'discord.js';
 const client = new Discord.Client({
 	partials: ["MESSAGE", "CHANNEL", "REACTION"],
-	intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"],
+	intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES", "GUILD_MESSAGE_REACTIONS"],
 });
 
 import * as Types from './types'
