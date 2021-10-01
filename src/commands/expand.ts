2714d9918b9ed90e10e19f4febd79840ccff8a3e
Maddy <ty.carlier@gmail.com>
2021-10-01 16:42:20 +1000

    setting descriptions apparently didn't work due to type casting! yay! also display description in pretty print / //expand

--- src/commands/expand.ts
+++ src/commands/expand.ts
@@ -12,11 +12,11 @@ export default new (class expand implements Types.Command {
 			return { reply: "Sorry, you must provide a valid reminder ID. Check out the `list` command!" };
 
 		var ret = Libs.reminders.prettyPrint({ ...list[id] });
-		ret.embeds[0].color = 0x0000ff;
-		ret.embeds[0].title = ret.embeds[0].description.split("`").join("");
-		ret.embeds[0].description = list[id].tag ? `Tag: \`${list[id].tag}\`` : "";
-		if (list[id].description)
-			ret.embeds[0].description = "\`" + list[id].description + "`";
-		return { reply: ret }
+		// ret.embeds[0].color = 0x0000ff;
+		// ret.embeds[0].title = ret.embeds[0].description.split("`").join("");
+		// ret.embeds[0].description = list[id].tag ? `Tag: \`${list[id].tag}\`` : "";
+		// if (list[id].description)
+		// 	ret.embeds[0].description = "\`" + list[id].description + "`";
+		return { reply: Libs.reminders.prettyPrint({ ...list[id] }) }
 	}
 })
