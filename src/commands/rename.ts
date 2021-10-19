d8c677bfe2fb46d91f210f36653ab328647cb2c3
Maddy <ty.carlier@gmail.com>
2021-10-19 15:08:37 +1100

    //rename now allows 'latest'

--- src/commands/rename.ts
+++ src/commands/rename.ts
@@ -2,12 +2,15 @@ import * as Types from "../types"
 
 export default new (class reload implements Types.Command {
 	name = "rename";
-	usage = "{reminder ID from list} {new name}";
+	usage = "{id | 'latest'} {new name}";
 	exec = async ({ user, args, Libs }: Types.CommandContext) => {
 		var list = await Libs.reminders.getAll(user._id);
 
 		var name = args || [];
-		var id = parseInt(name.shift()) - 1;	//lol
+		var id: number;
+		if (name[0] === "latest") id = Math.max.apply(0, list.map(x => x.remove_id)); 
+		else id = parseInt(name[0]) - 1;
+		name.shift();
 
 		if (!name || !list[id])
 			return { reply: "Sorry, you must provide a valid reminder ID, followed by the new name. Eg: `rename 1 vibe!`" };
