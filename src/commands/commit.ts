1888ef565b55216036c82b63ed7fc6668d043849
Maddy <ty.carlier@gmail.com>
2021-11-12 11:05:34 +0000

    updated //commit to actually show heroku version number and to not spam my logs if git can't be used

--- src/commands/commit.ts
+++ src/commands/commit.ts
@@ -2,21 +2,24 @@ import * as Types from "../types"
 
 import { execSync } from 'child_process'
 
+var cache: string = null;
+
 export default new (class timezone implements Types.Command {
 	name = "commit";
 	usage = "";
 	commandChainingLimit = 0;
 	getCommit = () => {
 		try {
-			return execSync("git rev-parse HEAD").toString().trim();
+			return execSync("git rev-parse HEAD").toString().trim().slice(0, 7);
 		}
 		catch (e) {
-			var string = process.env.HEROKU_SLUG_COMMIT + " : " + process.env.HEROKU_RELEASE_VERSION;
+			var string = process.env.HEROKU_SLUG_COMMIT.slice(0, 7) + " : " + process.env.HEROKU_RELEASE_VERSION;
 			return string === " : " ? "" : string;
 		}
 	}
 	exec = async (Context: Types.CommandContext) => {
-		const commit = this.getCommit().slice(0, 7);
+		const commit = cache || this.getCommit();
+		if (!cache) cache = commit;	//lets not spam our logs if git can't be used
 		return { reply: `Strawbean is currently running \`${commit ? commit : "could not get commit hash"}\`` }
 	}
 })
