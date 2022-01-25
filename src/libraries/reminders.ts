f6e8d57953816b40cc906365f0ec5ca588dbc1e3
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2022-01-25 18:04:31 +1100

    Added 1 month, 2 months, 1 year repeat options

--- src/libraries/reminders.ts
+++ src/libraries/reminders.ts
@@ -1,8 +1,8 @@
-import prettyMilliseconds from 'pretty-ms'
+import prettyMilliseconds from 'pretty-ms';
 
 import * as Mongodb from 'mongodb';
 import * as Types from '../types';
-import * as Discord from 'discord.js'
+import * as Discord from 'discord.js';
 
 class Reminders implements Types.Library {
 	name = "reminders";
@@ -12,19 +12,34 @@ class Reminders implements Types.Library {
 	adminDb: Mongodb.Admin = null;
 	deleteCache: {
 		[key: string]: {
-			[key: string]: Types.Reminder
-		}
+			[key: string]: Types.Reminder;
+		};
 	} = {};
 
 	timeOptions = {
-		"30 minutes": 30 * 60 * 1000,		//30 minutes
-		"1 hour": 60 * 60 * 1000,			//60 minutes,
-		"2 hours": 2 * 60 * 60 * 1000,		//2 hours
-		"6 hours": 6 * 60 * 60 * 1000,		//6 hours
-		"12 hours": 12 * 60 * 60 * 1000,	//12 hours
-		"24 hours": 24 * 60 * 60 * 1000,	//24 hours
-		"1 week": 7 * 24 * 60 * 60 * 1000,	//1 week
-	}
+		"30 minutes": () => 30 * 60 * 1000,
+		"1 hour": () => 60 * 60 * 1000,
+		"2 hours": () => 2 * 60 * 60 * 1000,
+		"6 hours": () => 6 * 60 * 60 * 1000,
+		"12 hours": () => 12 * 60 * 60 * 1000,
+		"24 hours": () => 24 * 60 * 60 * 1000,
+		"1 week": () => 7 * 24 * 60 * 60 * 1000,
+		"1 month": () => {
+			var now = new Date();
+			now.setMonth(now.getMonth() + 1);
+			return now.valueOf() - Date.now();
+		},
+		"2 months": () => {
+			var now = new Date();
+			now.setMonth(now.getMonth() + 2);
+			return now.valueOf() - Date.now();
+		},
+		"1 year": () => {
+			var now = new Date();
+			now.setFullYear(now.getFullYear() + 1);
+			return now.valueOf() - Date.now();
+		}
+	};
 
 	exec = async (Env: Types.Environment, client: Discord.Client) => {
 		client.on("interactionCreate", async (interaction: Discord.Interaction) => {
@@ -48,7 +63,7 @@ class Reminders implements Types.Library {
 
 		if (process.env.NODE_ENV === "production")
 			await client.application.commands.set(this.generateContextMenu());
-	}
+	};
 
 	interval = async (Env: Types.Environment, client: Discord.Client) => {
 		try {
@@ -100,9 +115,9 @@ class Reminders implements Types.Library {
 					content: `<@${reminder.owner}> : \`${short}\``,
 					embeds: [embed],
 					components: [reminder.repeating ? this.generateRemoveButton() : this.generateRepeatButtons()],
-				})
+				});
 
-				await this.collection.updateOne({ _id: reminder._id }, { $set: { msgAwaitReaction: msg.id } })
+				await this.collection.updateOne({ _id: reminder._id }, { $set: { msgAwaitReaction: msg.id } });
 			}
 			catch (e) {
 				reminder.repeating = false;
@@ -113,9 +128,9 @@ class Reminders implements Types.Library {
 			if (!reminder.repeating)
 				await this.remove(reminder.owner, reminder._id.toString());
 			else
-				await this.collection.updateOne({ _id: reminder._id }, { $set: { time: reminder.time + (reminder.repeating as number) } })
+				await this.collection.updateOne({ _id: reminder._id }, { $set: { time: reminder.time + (reminder.repeating as number) } });
 		}
-	}
+	};
 
 	generateContextMenu = (): Discord.ApplicationCommandData[] => {
 		return [
@@ -123,8 +138,8 @@ class Reminders implements Types.Library {
 				name: "Note this",
 				type: "MESSAGE",
 			},
-		]
-	}
+		];
+	};
 
 	handleContextMenu = async (interaction: Discord.ContextMenuInteraction) => {
 		var message = await interaction.options.getMessage("message") as Discord.Message;	//dumb api lol
@@ -137,10 +152,10 @@ class Reminders implements Types.Library {
 			setTime: message.createdTimestamp,
 			tag: "note",
 			description: null,
-		})
+		});
 
 		return await interaction.reply({ content: "I added this message to your notes! use `list note` to view them!", ephemeral: true });
-	}
+	};
 
 	generateRepeatButtons = (): Discord.MessageActionRow => {
 		return new Discord.MessageActionRow()
@@ -162,7 +177,7 @@ class Reminders implements Types.Library {
 						})),
 					])
 			);
-	}
+	};
 
 	generateRemoveButton = (): Discord.MessageActionRow => {
 		return new Discord.MessageActionRow()
@@ -172,7 +187,7 @@ class Reminders implements Types.Library {
 					.setLabel("Remove")
 					.setCustomId("remove")
 			);
-	}
+	};
 
 	handleButtons = async (interaction: Discord.Interaction) => {
 		var user = interaction.user;
@@ -182,7 +197,7 @@ class Reminders implements Types.Library {
 			var list = this.getRecentlyDeleted(user.id);
 			if (!list) throw 'bad';
 			var reminder = Object.values(list).find(x => {
-				return x.msgAwaitReaction === interaction.message.id
+				return x.msgAwaitReaction === interaction.message.id;
 			});
 			if (!reminder) throw 'bad';
 			if (reminder.owner !== user.id) throw 'bad';	//what
@@ -191,7 +206,7 @@ class Reminders implements Types.Library {
 			if (interaction.values[0] === "0")
 				time = reminder.time - reminder.setTime;
 			else
-				time = Object.values(this.timeOptions)[parseInt(interaction.values[0]) - 1];
+				time = Object.values(this.timeOptions)[parseInt(interaction.values[0]) - 1]();
 
 			await this.reinstate(reminder.owner, reminder._id.toString(), time);
 
@@ -203,7 +218,7 @@ class Reminders implements Types.Library {
 					url: reminder.url,
 					color: 0x00ff00,
 				}]
-			})
+			});
 
 			delete this.deleteCache[user.id][reminder._id.toString()];
 		}
@@ -215,7 +230,7 @@ class Reminders implements Types.Library {
 			await this.remove(reminder.owner, reminder._id.toString());
 			await interaction.reply("Reminder removed. Use `restore` to restore it.");
 		}
-	}
+	};
 
 	add = async (data: Types.Reminder) => {
 		var reminder = {
@@ -229,12 +244,12 @@ class Reminders implements Types.Library {
 			setTime: data.setTime,
 			url: data.url,
 			tag: data.tag,
-		}
+		};
 
 		var res = await this.collection.insertOne(reminder);
 		reminder._id = res.insertedId.toString();
 		return reminder;
-	}
+	};
 
 	addNote = async (data: Types.Note) => {
 		var note = {
@@ -244,49 +259,51 @@ class Reminders implements Types.Library {
 			setTime: data.setTime ? data.setTime : Date.now(),
 			description: data.description ? data.description.split("`").join("'") : null,
 			tag: "note",
-		}
+		};
 
 		var res = await this.collection.insertOne(note);
 		return note;
-	}
+	};
 
 	rename = async (user: string, id: string, newName: string) => {
 		return await this.collection.updateOne(
 			{ _id: id, owner: user },
 			{ $set: { name: newName } }
 		);
-	}
+	};
 
 	setDescription = async (user: string, id: string, description: string) => {
 		return await this.collection.updateOne(
 			{ _id: id, owner: user },
 			{ $set: { description: description } }
 		);
-	}
+	};
 
 	setTag = async (user: string, id: string, tag: string) => {
 		return await this.collection.updateOne(
 			{ _id: id, owner: user },
 			{ $set: { tag: tag } }
 		);
-	}
+	};
 
 	setTime = async (user: string, id: string, seconds: number, offset: number) => {
 		if (!offset) offset = Date.now();
 		return await this.collection.updateOne(
 			{ _id: id, owner: user },
-			{ $set: {
-				time: offset + seconds,
-				setTime: offset
-			} }
-		)
-	}
+			{
+				$set: {
+					time: offset + seconds,
+					setTime: offset
+				}
+			}
+		);
+	};
 
 	getAll = async (user: string): Promise<Types.Reminder[]> => {
 		var cursor = await this.collection.find({ owner: user });
 		var reminders = (await cursor.toArray()) as Types.Reminder[];
 		return reminders.map((x, i) => ({ ...x, remove_id: i }));
-	}
+	};
 
 	reinstate = async (user: string, id: string, time: number, offset?: number) => {
 		offset = offset || Date.now();
@@ -296,17 +313,17 @@ class Reminders implements Types.Library {
 		reminder.setTime = offset;
 		delete this.deleteCache[user][id];
 		return await this.add(reminder);
-	}
+	};
 
 	remove = async (user: string, id: string) => {
 		if (!this.deleteCache[user]) this.deleteCache[user] = {};
 		this.deleteCache[user][id.toString()] = await this.collection.findOne({ _id: new Mongodb.ObjectId(id), owner: user }) as Types.Reminder;
 		return await this.collection.deleteOne({ _id: new Mongodb.ObjectId(id), owner: user });
-	}
+	};
 
 	getRecentlyDeleted = (user: string) => {
 		return this.deleteCache[user];
-	}
+	};
 
 	prettyPrint = (reminder: Types.Reminder | Types.Note): Discord.MessageEditOptions => {
 		if (reminder.description) {
@@ -323,7 +340,7 @@ class Reminders implements Types.Library {
 						text: reminder.tag
 					},
 				}]
-			}
+			};
 		}
 
 		return {
@@ -339,8 +356,8 @@ class Reminders implements Types.Library {
 					text: reminder.tag
 				},
 			}]
-		}
-	}
+		};
+	};
 }
 
 export default new Reminders();
